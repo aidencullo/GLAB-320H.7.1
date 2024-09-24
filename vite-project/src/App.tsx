f173938c2253a78741884223f5ec82a203767576
Aiden Cullo <aidencullo@gmail.com>
2024-09-24 10:19:25 -0400

    error handling on getMovies

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -6,27 +6,30 @@ import MovieDisplay from "./components/MovieDisplay";
 import Form from "./components/Form";
 
 export default function App() {
-  const apiKey = "98e3fb1f";
+    const apiKey = "98e3fb1f";
 
-  const [movie, setMovie] = useState(null);
+    const [movie, setMovie] = useState(null);
 
-  const getMovie = async (searchTerm) => {
-    const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    const getMovie = async(searchTerm) => {
+	try {
+	    const response = await fetch(
+		`http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+	    );
+	    const data = await response.json();
+	    setMovie(data);
+	} catch(e) {
+	    console.error(e)
+	}
+    }
+    // This will run on the first render but not on subsquent renders
+    useEffect(() => {
+	getMovie("Clueless");
+    }, []);
+
+    return (
+	<div className="App">
+	    <Form moviesearch={getMovie} />
+	    <MovieDisplay movie={movie} />
+	</div>
     );
-    const data = await response.json();
-    setMovie(data);
-  };
-
-  // This will run on the first render but not on subsquent renders
-  useEffect(() => {
-    getMovie("Clueless");
-  }, []);
-
-  return (
-    <div className="App">
-      <Form moviesearch={getMovie} />
-      <MovieDisplay movie={movie} />
-    </div>
-  );
 }
